a30f2cf0e3864dca319144627a9fcb0d8fa7e18e
TranLang28 <1612331@student.hcmus.edu.vn>
2019-04-20 13:16:06 +0700

    Giao diện Sign in, Sign up

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET Logn In page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Log in' });
 });
+
+
 
 module.exports = router;
