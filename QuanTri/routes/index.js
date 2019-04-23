3f80dedd1701508cab90cbcaee924ad4696813cd
TranLang28 <1612331@student.hcmus.edu.vn>
2019-04-23 17:47:01 +0700

    Chuyển HTML SANG EJS BẰNG EXPRESS
    Giao diện danh sách cửa hàng, chờ duyệt, sản phẩm, tài khoản; đơn hàng, thay đổi thông tin tài khoản

--- QuanTri/routes/index.js
+++ QuanTri/routes/index.js
@@ -9,4 +9,25 @@ router.get('/', function(req, res, next) {
 router.get('/danh-sach-tai-khoan', function(req, res, next) {
   res.render('danh-sach-tai-khoan', { title: 'Express' });
 });
+
+router.get('/danh-sach-cho-duyet', function(req, res, next) {
+  res.render('danh-sach-cho-duyet', { title: 'Express' });
+});
+
+router.get('/thay-doi-thong-tin', function(req, res, next) {
+  res.render('thay-doi-thong-tin', { title: 'Express' });
+});
+
+router.get('/danh-sach-cua-hang', function(req, res, next) {
+  res.render('danh-sach-cua-hang', { title: 'Express' });
+});
+
+router.get('/danh-sach-san-pham', function(req, res, next) {
+  res.render('danh-sach-san-pham', { title: 'Express' });
+});
+
+router.get('/don-hang', function(req, res, next) {
+  res.render('don-hang', { title: 'Express' });
+});
+
 module.exports = router;
