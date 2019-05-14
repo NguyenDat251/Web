1aac502b50c53f34e2a32c57378c808f0291552a
TranLang28 <1612331@student.hcmus.edu.vn>
2019-05-14 11:47:12 +0700

    Connect MongoDB

--- eshop/controllers/San_pham_controller.js
+++ eshop/controllers/San_pham_controller.js
@@ -1,24 +1,24 @@
 const product = require('../models/San_pham');
 
 exports.info = async (req, res, next) => {
-    const data = {
-        tensp: 'iPhone XS Max 64 GB',
-        brand: 'Apple',
-        price: '28,790,000',
-        color: ['Bạc', 'Vàng', 'Xám'],
-        shortInfo: ['Hệ điều hành: iOS 12', 'RAM: 4 GB', 'ROM: 64 GB', 'Chip xử lý: A12 Bionic 64-bit 7nm'],
-        info: {
-            screen: '6.5 inches',
-            ram: '4 GB',
-            rom: '64 GB',
-            frontCamera: '7 MP, f / 2.2, 32mm',
-            backCamera: '12 MP',
-            os: 'iOS 12',
-            sim: '1',
-            pin: '3174 mAh'
-        }
-    };
-    const products = await product.detail('5ccaf18c677f2cc0f6a734ff');
+    // const data = {
+    //     tensp: 'iPhone XS Max 64 GB',
+    //     brand: 'Apple',
+    //     price: '28,790,000',
+    //     color: ['Bạc', 'Vàng', 'Xám'],
+    //     shortInfo: ['Hệ điều hành: iOS 12', 'RAM: 4 GB', 'ROM: 64 GB', 'Chip xử lý: A12 Bionic 64-bit 7nm'],
+    //     info: {
+    //         screen: '6.5 inches',
+    //         ram: '4 GB',
+    //         rom: '64 GB',
+    //         frontCamera: '7 MP, f / 2.2, 32mm',
+    //         backCamera: '12 MP',
+    //         os: 'iOS 12',
+    //         sim: '1',
+    //         pin: '3174 mAh'
+    //     }
+    // };
+    const data = await product.detail('5ccaf18c677f2cc0f6a734ff');
     console.log(products);
     res.render('San_pham', {title: 'products.name', data})
 };
--- eshop/routes/index.js
+++ eshop/routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+const ObjectId = require('mongodb').ObjectId;
+var San_pham_controller = require('../controllers/San_pham_controller');
+//const MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,10 +24,33 @@ router.get('/login', function(req, res, next) {
   res.render('Dang_nhap', { title: 'Cửa hàng' });
 });
 
-router.get('/productInfo', function(req, res, next) {
-  res.render('San_pham', { title: 'Sản phẩm' });
-});
 
+var url = 'mongodb+srv://dat:dat251@cluster0-jslyd.mongodb.net/WebDB?retryWrites=true';
+//var url = 'mongodb://localhost:27017';
+router.get('/productInfo', async function(req, res, next) {
+    // var name = [];
+    // const data = null;
+    await mongo.connect(url,  function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        var dbo = db.db();
+
+        console.log("Connected successfully to database");
+
+        var data1 = dbo.collection("SanPham").find({});//.toArray();
+        console.log("Connected successfully to colecction");
+        var data = {};
+        //res.render('San_pham',{title: 'products.name', data});
+        console.log(data1);
+        data1.forEach(row => {
+            //console.log(row);
+            data=row;
+            console.log(data);
+            res.render('San_pham',{title: 'products.name', data});
+        });
+
+    })
+});
 router.get('/bucket', function(req, res, next) {
   res.render('Gio_hang', { title: 'Giỏ hàng' });
 });
